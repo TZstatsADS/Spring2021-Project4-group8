b70462f1d05fa600b309e98056e865a9f38ed724
Dennis Shpits <ds3922@columbia.com>
2021-04-05 16:32:25 -0400

    final push before merge

--- lib/propensity_matching.R
+++ lib/propensity_matching.R
@@ -14,3 +14,29 @@ get_match_obj <- function(Any_Dim_Data, propensity_score=NULL){
   return(matchit.out)
   
 }
+
+#function returns the lm fit using treatment-covariate interactions
+get_ATE_using_treatment_covariate_interactions <- function(matches_obj, num_covs){
+  
+  #Estimating a covariate-adjusted marginal effect
+  #with treatment-covariate interactions
+  #Create a new dataset for centered variables
+  md_cen <- matches_obj
+  
+  
+  covs_to_center <- seq(1, num_covs, by=1)
+  covs_to_center=paste0("V",covs_to_center)
+  
+
+  md_cen[covs_to_center] <- scale(matches_obj[covs_to_center], 
+                                  scale = FALSE)
+  
+  
+  dynamic_formula <- paste(paste("Y ~ A *(", paste(covs_to_center, collapse="+")),")")
+  
+  
+  #Fit the model with every covariate interacting with treatment
+  fit2 <- lm(dynamic_formula,
+             data = md_cen, weights = weights)
+  return(fit2)
+}
